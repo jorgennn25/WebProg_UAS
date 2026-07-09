42fec1ed79f32551a8c377e306f004c734c8dda1
Jorgen <nichongedab@gmail.com>
2026-07-09 22:15:10 +0700

    Revisi

--- index.php
+++ index.php
@@ -11,14 +11,10 @@
             font-family: Times New Roman, sans-serif;
             margin: 20px;
         }
-        
-        /* Menggunakan teknik Float dari materi Week 11 */
         .panel {
-            float: left; /* Kotak akan mengambang ke kiri sejajar */
-            width: 48%;  /* Membagi layar hampir setengah-setengah */
+            float: left; 
+            width: 48%;  
         }
-        
-        /* Memberikan jarak antara kotak kiri dan kanan */
         #panel-inisialisasi {
             margin-right: 2%;
         }
@@ -26,19 +22,14 @@
             margin-bottom: 10px;
         }
         .form-group label {
-            /* Menggunakan inline-block dari materi Week 10 untuk label form */
             display: inline-block;
             width: 120px;
         }
-        
-        /* Menggunakan clear: both dari materi Week 11 slide 17 */
-        /* Berfungsi agar elemen Denah di bawahnya tidak ikut menempel di atas */
         #denah-area {
             clear: both; 
             padding-top: 20px;
         }
 
-        /* Aturan layout Tabel dari materi Week 08 */
         table {
             width: 100%; 
             border-collapse: collapse;
@@ -49,8 +40,6 @@
             height: 30px;
             text-align: center;
         }
-        
-        /* Class CSS untuk warna saat statusnya Unavailable */
         .unavailable {
             background-color: gray;
         }
@@ -85,7 +74,7 @@
                 </div>
                 <div class="form-group">
                     <label>Jenis</label>
-                    <input type="radio" name="jenis" value="available" checked> Available
+                    <input type="radio" name="jenis" value="available"> Available
                     <input type="radio" name="jenis" value="unavailable"> Unavailable
                 </div>
                 <button id="btn-simpan">Simpan</button>
@@ -99,17 +88,12 @@
     </div>
 
     <script>
-        $(document).ready(function() {
-            
-            // 1. Fungsi saat tombol Generate ditekan (Materi Week 13 & Week 08)
+        $(function() {
             $("#btn-generate").click(function() {
                 var baris = $("#jml-baris").val();
                 var kolom = $("#jml-kolom").val();
-
-                if (baris > 0 && kolom > 0) {
-                    var tableHtml = "<table>";
-                    
-                    // Melakukan looping untuk membuat baris dan kolom
+                
+                var tableHtml = "<table>";
                     for (var i = 1; i <= baris; i++) {
                         tableHtml += "<tr>";
                         for (var j = 1; j <= kolom; j++) {
@@ -119,32 +103,20 @@
                     }
                     tableHtml += "</table>";
                     
-                    // Mencetak html tabel ke dalam div
                     $("#table-container").html(tableHtml);
-                } else {
-                    alert("Harap masukkan Jumlah Baris dan Jumlah Kolom!");
-                }
             });
-
-            // 2. Fungsi saat tombol Simpan ditekan (Materi Week 13 - Client Scripting)
             $("#btn-simpan").click(function() {
                 var tBaris = $("#target-baris").val();
                 var tKolom = $("#target-kolom").val();
                 var jenis = $("input[name='jenis']:checked").val();
-
-                if (tBaris !== "" && tKolom !== "") {
-                    // Menyusun ID tujuan (misal: #cell-2-3)
+                
                     var targetId = "#cell-" + tBaris + "-" + tKolom;
                     
-                    // Mengubah class sesuai jenis okupansi yang dipilih
                     if (jenis === "unavailable") {
                         $(targetId).addClass("unavailable");
                     } else {
                         $(targetId).removeClass("unavailable");
                     }
-                } else {
-                    alert("Harap isi input Baris dan Kolom Okupansi!");
-                }
             });
 
         });
